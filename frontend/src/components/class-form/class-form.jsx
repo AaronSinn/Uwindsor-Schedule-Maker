5bdc1b4e42ff03d717817a721ef7b6cc4a5fab5a
Aaron Sinn <97710861+AaronSinn@users.noreply.github.com>
2024-11-05 13:34:06 -0500

    Update class-form.jsx

--- frontend/src/components/class-form/class-form.jsx
+++ frontend/src/components/class-form/class-form.jsx
@@ -4,6 +4,8 @@ export default function ClassForm(){
     return<>
         <div className='class-form-container'>
             <h3>Add Class</h3>
+            {/* Use Dropdown with filter */}
+            {/* Have 2 divs. One for selecting the course. Another for selecting the section(s). */}
             <input type="dropdown" name="class" id="" />
         </div>
     </>
