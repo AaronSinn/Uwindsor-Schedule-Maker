192e192f16af0b0d1ab5830424192fa618435c95
Aaron Sinn <97710861+AaronSinn@users.noreply.github.com>
2024-12-03 15:29:34 -0500

    Update ParseClassInfo.py

--- Uwindsor Data Parser/ParseClassInfo.py
+++ Uwindsor Data Parser/ParseClassInfo.py
@@ -1,24 +1,27 @@
 import fitz
 import re
-import datetime
-from Models.LectureModel import LectureModel
-from Models.SectionModel import SectionModel
+from sqlalchemy.orm import sessionmaker
 
 IS_PROF_AND_ROOM_STATED = False
 
 doc = fitz.open("PDFs/fall_2024_ugrd_timetable.pdf")
-#file = open('test_file.txt', "wt+")
 
 for page in doc:
     text = page.get_text()
-    #file.write(page.get_text())
-    #print(page.get_text())
 
-    courseRegex = re.compile(r"(\w{4}-\s*?\w{4}\w?) \(-\)\n(.*)\n(Section \d{1,2} [\s\S]*?)\w{4}-\s*?\w{4}\w?")
+    courseRegex = re.compile(r"(\w{4}-\s*?\w{4}\w?) \(-\)\n(.*)\n(Section \d{1,2} [\s\S]*?)(?=\n\w{4}-\s*?\w{4}\w?|$)")
     courses = courseRegex.findall(text)
 
-    file = open('course_text.txt', "wt+")
-
     for course in courses:
-        file.write(str(course) + '\n')
         print(course, '\n')
+
+        course_code = course[0]
+        course_title = course[1]
+        section_details = course[2]
+
+        sectionRegex = re.compile(r"""((Section /d{1,2})\n3.00?\n([LECLAB])\n(\D*?)\n?(\d\d:\d\d \w\w)?\n?(\d\d:\d\d)?\s?\n?([PMAM]{2})?\n?
+                                  (\d\d\d\d)-\n(\d\d)-(\d\d)\n(\d\d\d\d)-\n(\d\d)-(\d\d))""")
+        sections = sectionRegex.findall(section_details)
+
+        for section in sections:
+            print('SECTION: ', section, '\n')
