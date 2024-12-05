7015dee3a869998e512638461cc36c35cbf151fe
Aaron Sinn <97710861+AaronSinn@users.noreply.github.com>
2024-12-05 15:19:13 -0500

    Update Course.cs

--- backend/Models/Course.cs
+++ backend/Models/Course.cs
@@ -1,13 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 
 namespace backend.Models;
 
 public class Course
 {
     public int Id { get; set; }
-    public string Title { get; set; } = string.Empty;
     public string Code { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
     public ICollection<Section>? Sections { get; set; }//Collection of Labs and Lecs
 }
