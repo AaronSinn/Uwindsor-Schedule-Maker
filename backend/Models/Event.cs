c74ddbfd5ba763b590e9ebda0aa86203f1443f01
Aaron Sinn <97710861+AaronSinn@users.noreply.github.com>
2024-12-05 15:19:28 -0500

    Update Event.cs

--- backend/Models/Event.cs
+++ backend/Models/Event.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 
 namespace backend.Models;
 public class Event
 {
     public int Id { get; set; }
+    [MaxLength(30)]
     public string Title { get; set; } = string.Empty;
     public string[] Days { get; set; } = [];
     public string StartTime { get; set; } = string.Empty; //SQLite can.t store DateTime
     public string EndTime { get; set; } = string.Empty; //SQLite can.t store DateTime    
+    [MaxLength(30)]
     public string Location { get; set; } = string.Empty;
 }
