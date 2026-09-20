466a5ab8d11c9ceb20745fbcac661d99413191e4
AaronSinn <aaron.sinn@me.com>
2026-09-19 23:11:51 -0400

    Revert "Reapply "first commit""
    This reverts commit 2183390d4c14b304acae00dacb9480fa9f9d608b.

---
+++ backend/Program.cs
@@ -0,0 +1,43 @@
+using backend.Database;
+//using backend.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add the Database
+builder.Services.AddSqlite<DatabaseContext>("Data Source=database.db");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(x => x
+     .AllowAnyOrigin()
+     .AllowAnyMethod()
+     .AllowAnyHeader()
+     .WithOrigins("https://localhost:5150", "https://uwindsor-schedule-maker-cjg6eqcccyd9edeh.canadacentral-01.azurewebsites.net")
+     .SetIsOriginAllowed(origin => true));
+
+app.UseCors("AllowSpecificOrigins");
+
+app.UseAuthorization();
+app.MapControllers();
+app.UseHttpsRedirection();
+
+// Add the CreateDbIfNotExists method call
+// Uncomment if you dont have a db
+//app.CreateDbIfNotExists();
+
+// serves from wwwroot
+app.UseStaticFiles();
+app.MapFallbackToFile("index.html");
+
+app.Run();
